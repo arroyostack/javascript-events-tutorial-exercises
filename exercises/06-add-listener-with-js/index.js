9c55cba59099ba315666c0f80af70a55b5675d62
El Hadj Mbengue Arroyo <109061270+arroyostack@users.noreply.github.com>
2023-06-23 14:36:09 +0000

    exercise finished

--- exercises/06-add-listener-with-js/index.js
+++ exercises/06-add-listener-with-js/index.js
@@ -1,6 +1,12 @@
 window.onload = function myLoadFunction() {
 	alert("The website just finished loading!");
 	//some code here
+	const greenButton = document.getElementById('theGreen');
+
+	greenButton.addEventListener('click', () => {
+		alert('whoohoo');
+	});
 };
+
 
 //the listener function here
